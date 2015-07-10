dc9b0d347076937e945cfd1f866268f1199bbfc8
Liz Joseph <u10075268@tuks.co.za>
2015-07-10 13:59:35 +0200

    Added splash screen

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET splash page. */
 router.get('/', function(req, res, next) {
+  res.render('splash', { title: 'Kalafong PIMS' });
+});
+
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Kalafong PIMS' });
 });
 
@@ -16,5 +22,9 @@ router.get('/add', function(req, res, next) {
     res.render('add', { title: 'Kalafong PIMS - Add New User' });
 });
 
-
+/* GET login page. */
+router.get('/form', function(req, res, next) {
+    res.render('formBuild', { title: 'Form Builder' });
+	
+});
 module.exports = router;
