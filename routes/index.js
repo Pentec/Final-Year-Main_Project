bfefc76071a8bcab8d53181239e441b1e15bf0a7
lizjoseph <u10075268@tuks.co.za>
2015-07-16 15:45:13 +0200

    Added in Schema Code to recify connection problem

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,109 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('countdown', { title: 'Kalafong PIMS: Coming Soon' });
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+mongoose.connect('mongodb://Admin:qYMqsW5Z@ds033601.mongolab.com:33601/pentec_pims');
+
+
+var Form = new Schema({
+    form_name			: String,
+    data			: String,
+    is_deleted			: Boolean
 });
 
-/* GET login page. */
+var Users = new Schema({
+    username				: String,
+    surname				: String,
+    email				: String,
+    profile_pic				: String,
+    user_rights				: Number,
+    password				: String,
+    department				: String,
+    staff_type				: String
+});
+
+mongoose.model('users', Users);
+mongoose.model('forms', Form);
+var Form = mongoose.model('forms');
+var User = mongoose.model('users');
+var login = require('pims-login');
+
+/* GET splash page. */
+router.get('/', function(req, res, next) {
+  res.render('splash', { title: 'Kalafong PIMS' });
+});
+
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Kalafong PIMS' });
+});
+
+/*******************************LOGIN BELOW**********************************************/
+
+/* GET login page */
 router.get('/login', function(req, res, next) {
     res.render('login', { title: 'PIMS Login Page' });
 });
+
+ /*POST login page.*/
+router.post('/login', function(req, res, next) {
+    var username = req.body.userid;
+    var password = req.body.pswd;
+    console.log("Hello \n" + req.body.userid);
+    console.log("Hello Again \n" + req.body.pswd);
+
+    login.authenticate(username, password, function(found) {
+        console.log('Finished calling authenticate. \n' + found);
+         if(found)
+         {
+             console.log("its true");
+             res.redirect('home');
+         }
+         else
+         {
+             console.log("its false");
+             res.redirect('login');
+         }
+    });
+
+});
+/*******************************LOGIN ABOVE**********************************************/
 
 /* Add New User page */
 router.get('/add', function(req, res, next) {
     res.render('add', { title: 'Kalafong PIMS - Add New User' });
 });
 
-/*Countdown Page*/
-router.get("/dashboard", function(req, res, next){
-    res.render('index', { title : 'PIMS Dashboard'});
+
+/* Add New User to database */
+router.post('/create', function(req, res) {
+  new User({username : req.body.username,surname : req.body.surname,email : req.body.email,user_rights : req.body.user_rights,password : req.body.password,department : req.body.department,staff_type : req.body.staff_type })
+  .save(function(err, users) {
+  console.log("New user added");
+    res.redirect('add');
+	
+  });
+});
+
+/* GET login page. */
+router.get('/form', function(req, res, next) {
+    res.render('formBuild', { title: 'Form Builder' });
+ 
+});
+
+router.post('/formsave', function(req, res) {
+    var object = JSON.stringify(req.body);
+    console.log(object);
+
+    new Form({form_name : "new form",data : object ,is_deleted : false})
+        .save(function(err, forms) {
+            console.log("New form added");
+            res.redirect('formBuild');
+
+        });
+
 });
 
 
