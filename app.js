f411838c10b4b9e9d6672231ca96c807df8e774d
RuthOjo <RuthOjo@users.noreply.github.com>
2015-07-22 18:04:02 +0200

    Added express-validator usage

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'html'))); //for html forms
 app.use(express.static(path.join(__dirname, 'public')));
