c7df69dccde8109300f05275c41172026b01fbe7
lindelo <u12002862@tuks.co.za>
2015-09-19 15:25:46 +0200

    Added code documentation

--- routes/forms/gynaecology_surgery.js
+++ routes/forms/gynaecology_surgery.js
@@ -3,6 +3,14 @@ var router = express.Router();
 var saveFunctions  = require('../../controllers/forms/submitForm');
 
 router.post('/', function(req, res, next) {
-    saveFunctions.submitGynaecologySurgery(req);
+    var success = saveFunctions.submitGynaecologySurgery(req);
+    console.log(success);
+
+    if(!success){
+        res.redirect('/gynae_surgery.html');
+    }
+    else{
+        res.redirect('/myAdminSpace');
+    }
 });
 module.exports = router;
