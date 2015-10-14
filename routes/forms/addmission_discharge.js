cf9d1223920823d01a0dacb81024e5718a4cec09
MariaQumayo <mariaqumayo38@gmail.com>
2015-10-14 16:22:48 +0100

    work on user dynamics

--- routes/forms/addmission_discharge.js
+++ routes/forms/addmission_discharge.js
@@ -4,13 +4,20 @@ var submitFunctions  = require('../../controllers/forms/submitForm');
 var saveFunctions  = require('../../controllers/forms/saveForm');
 
 router.post('/', function(req, res, next) {
-    if(req.body.isNotCompeleted == true)
-    {
-        saveFunctions.saveAdmissionDischarge(req, req.session.username);
-    }
-    else
-    {
-        submitFunctions.submitAdmissionDischarge(req);
-    }
+  //  if(req.body.isNotCompeleted == true)
+  //  {
+     //   saveFunctions.saveAdmissionDischarge(req, req.session.username);
+   // }
+   // else
+   // {
+      //  submitFunctions.submitAdmissionDischarge(req);
+   // }
+
+    if(onSubmitValid() == true){
+
+       res.redirect('/splash');
+   }
+
+
 });
 module.exports = router;
--- routes/forms/gynaecology_surgery.js
+++ routes/forms/gynaecology_surgery.js
@@ -4,14 +4,23 @@ var submitFunctions  = require('../../controllers/forms/submitForm');
 var saveFunctions  = require('../../controllers/forms/saveForm');
 
 router.post('/', function(req, res, next) {
+
+   // if(onSubmitValid() == true){
+
+       // res.redirect('/splash');
+   // }
+
+
+
+
+
     if(req.body.isNotCompeleted == true)
     {
         saveFunctions.saveGynaecologySurgery(req, req.session.username);
-    }
     else
     {
     var success = submitFunctions.submitGynaecologySurgery(req);
-    console.log(success);
+   console.log(success);
 
     if(!success){
         res.redirect('/gynae_surgery.html');
