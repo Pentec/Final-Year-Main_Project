18f7b127fb486e6ff61a3c603e5af8fff2a26a82
Trevor Austin <taustin.swim@gmail.com>
2015-10-14 18:33:21 +0200

    Fixed UserManual lik

--- public/javascripts/space/myAdminSpace.js
+++ public/javascripts/space/myAdminSpace.js
@@ -2,6 +2,7 @@
  * Created by Trevor on 2015/10/07.
  */
 var lastClicked = null;
+var prevWidth = null;
 $(function(){
     init();
 });
@@ -89,12 +90,13 @@ function rotate(clas){
 
 
 function init(){
-
+    var width = $(document).width();
+    if(width == prevWidth){
+        return;
+    }
     var pills  = $(".item").children();
     var totalPills = $(".item > a").length + $(".item > ul").length;
     $(".item").remove();
-    var width = $(document).width();
-
     switch(true){
         case width < 600:
             createItems(totalPills, pills, totalPills);
@@ -142,6 +144,7 @@ function init(){
             }, 450
         );
     });
+    prevWidth = width;
 }
 
 function createItems(noOfItems, pills, totalPills){
